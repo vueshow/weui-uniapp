ee7b3adeef98c123c39341782f347f93fc9a5161
温柄焱 <wenbingyan@gmail.com>
2026-04-19 15:28:00 +0800

    fix: deploy h5 demo under pages base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.UNI_BASE || '/',
   server: {
     host: '127.0.0.1',
     port: 5174,
