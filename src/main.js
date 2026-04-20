a296c726f0d68e1a13471220b36ab4d90100e3fc
温柄焱 <wenbingyan@gmail.com>
2026-04-20 20:05:58 +0800

    fix: restore h5 demo component registration

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import {
 	createSSRApp
 } from "vue";
 import App from "./App.vue";
+// #ifndef MP
+import WeuiUni from "../uni_modules/weui-uni";
+// #endif
 export function createApp() {
 	const app = createSSRApp(App);
+	// #ifndef MP
+	app.use(WeuiUni);
+	// #endif
 	return {
 		app,
 	};
